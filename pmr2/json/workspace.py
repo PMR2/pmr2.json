115c26f240c53fb00c843adc376c22cb30ac41fc
Tommy Yu <tommy.yu@auckland.ac.nz>
2013-01-21 19:27:06 +1300

    Workspace listing in JSON.

--- pmr2/json/workspace.py
+++ pmr2/json/workspace.py
@@ -1,5 +1,7 @@
 import json
 
+from Products.CMFCore.utils import getToolByName
+
 from pmr2.json.mixin import SimpleJsonFormMixin, SimpleJsonAddFormMixin
 from pmr2.app.workspace.browser.browser import WorkspaceStorageCreateForm
 from pmr2.app.workspace.browser.browser import WorkspacePage
@@ -10,6 +12,26 @@ from pmr2.json.mixin import JsonPage
 class JsonWorkspaceStorageCreateForm(SimpleJsonAddFormMixin,
         WorkspaceStorageCreateForm):
     pass
+
+
+class JsonWorkspaceContainerList(JsonPage):
+
+    def render(self):
+        workspace = self.context
+        catalog = getToolByName(workspace, 'portal_catalog')
+
+        query = {
+            'portal_type': 'Workspace',
+            'path': [
+                u'/'.join(workspace.getPhysicalPath()),
+            ],
+            'sort_on': 'sortable_title',
+        }
+        results = catalog(**query)
+
+        keys = ['title', 'target']
+        values = [dict(zip(keys, (i.Title, i.getURL(),))) for i in results]
+        return json.dumps(values)
 
 
 class JsonWorkspacePage(JsonPage, WorkspacePage):
