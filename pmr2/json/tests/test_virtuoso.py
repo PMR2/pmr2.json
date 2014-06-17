14ac4b874ac03e098dee395e66466ab374ee1109
Tommy Yu <tommy.yu@auckland.ac.nz>
2014-06-17 17:06:39 +1200

    Migrating virtuoso tests to match upstream

--- pmr2/json/tests/test_virtuoso.py
+++ pmr2/json/tests/test_virtuoso.py
@@ -7,7 +7,7 @@ import zope.component
 from Products.PloneTestCase import ptc
 
 from pmr2.virtuoso.interfaces import IWorkspaceRDFInfo
-from pmr2.virtuoso.tests.base import WorkspaceRDFTestCase
+from pmr2.virtuoso.tests.layer import PMR2_VIRTUOSO_INTEGRATION_LAYER
 from pmr2.json.tests import base
 
 try:
@@ -18,23 +18,22 @@ except ImportError:
 
 
 @unittest.skipUnless(_virtuoso, 'pmr2.virtuoso is unavailable')
-class VirtuosoTestCase(WorkspaceRDFTestCase):
+class VirtuosoTestCase(unittest.TestCase):
     """
     Testing functionalities of forms that don't fit well into doctests.
     """
 
-    def afterSetUp(self):
-        pass
+    layer = PMR2_VIRTUOSO_INTEGRATION_LAYER
 
     def test_base_render(self):
         request = base.TestRequest()
-        context = self.portal.workspace['virtuoso_test']
+        context = self.layer['portal'].workspace['virtuoso_test']
         f = virtuoso.WorkspaceRDFInfoEditForm(context, request)
         results = json.loads(f())
         self.assertEqual(results["fields"].keys(), ['paths'])
 
     def test_workspace_rdf_edit_form_submit(self):
-        context = self.portal.workspace['virtuoso_test']
+        context = self.layer['portal'].workspace['virtuoso_test']
         request = base.TestRequest(method='POST',
             stdin=StringIO('{"fields":{"paths":["simple.rdf"]},'
                 '"actions":{"apply":1}}'))
