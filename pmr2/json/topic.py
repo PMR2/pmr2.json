925a02805f751b411f2423ce7c48219a93c86546
Tommy Yu <tommy.yu@auckland.ac.nz>
2015-01-06 17:46:37 +1300

    Check for necessity of `/view` in href
    - Make the output check type against typesUseViewActionInListings in
      site properties, like the default listing.

--- pmr2/json/topic.py
+++ pmr2/json/topic.py
@@ -7,9 +7,17 @@ from pmr2.json.hal.core import generate_hal
 class ATCTTopicJsonPage(JsonPage):
 
     def render(self):
+        siteprop = self.context.portal_properties.site_properties
+        use_view_action = getattr(siteprop, 'typesUseViewActionInListings', ())
+
+        def view_url(item):
+            if item.portal_type in use_view_action:
+                return item.getURL() + '/view'
+            return item.getURL()
+
         results = self.context.queryCatalog()
         links = [{
-            'href': i.getURL(),
+            'href': view_url(i),
             'label': i.Title,
         } for i in results]
 
