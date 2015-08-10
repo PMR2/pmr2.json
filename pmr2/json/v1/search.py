f1ed445814cfc1b2a21bc57717db2c405cf5a467
Tommy Yu <tommy.yu@auckland.ac.nz>
2015-08-10 19:25:15 +1200

    JSON+Collection based search form, with options.

--- pmr2/json/v1/search.py
+++ pmr2/json/v1/search.py
@@ -4,29 +4,48 @@ from pmr2.json.collection.mixin import JsonCollectionPage
 from pmr2.json.mixin import extractRequestObj
 
 
+def index_values(catalog, name, **kw):
+    return [{
+        "value": value
+    } for value in catalog._catalog.getIndex(name).uniqueValues()]
+
+
 class JsonSearchPage(JsonCollectionPage):
 
-    valid_fields = ('SearchableText',)
+    valid_fields = (
+        'SearchableText', 'Title', 'Description', 'Subject', 'portal_type',
+    )
+    # associated functions to resolve the choices in these fields.
+    choice_fields = {
+        'portal_type': index_values,
+        'Subject': index_values,
+    }
 
     def _build_template(self):
         data = []
-        for f in self.valid_fields:
-            data.append({
-                'name': f,
-                'prompt': f,
+        for field_name in self.valid_fields:
+            item = {
+                'name': field_name,
+                'prompt': field_name,
                 # 'description': f,
-                'value': self.request.get(f, ''),
+                'value': self.request.get(field_name, ''),
                 # 'options': options,
-            })
+            }
+            f_options = self.choice_fields.get(field_name)
+            if f_options:
+                item['options'] = f_options(self.catalog, field_name)
+            data.append(item)
         return data
 
     def update(self):
+        self.catalog = getToolByName(self.context, 'portal_catalog')
+
         super(JsonSearchPage, self).update()
+
         query = extractRequestObj(self.request)
         if not query:
             # process this to collection items
             # self.results = self.catalog(**query)
-            self.catalog = getToolByName(self.context, 'portal_catalog')
             self.results = []
 
         self._jc_template = self._build_template()
--- pmr2/json/v1/tests/test_search.py
+++ pmr2/json/v1/tests/test_search.py
@@ -2,8 +2,13 @@ import unittest
 import json
 
 import zope.component
+from Products.CMFCore.utils import getToolByName
 
 from zope.publisher.browser import TestRequest
+from plone.app.testing import login
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import TEST_USER_NAME
 
 from pmr2.json.v1 import search
 from pmr2.json.testing import layer
@@ -15,6 +20,7 @@ class SearchTestCase(unittest.TestCase):
     """
 
     layer = layer.COLLECTION_JSON_LAYER
+    maxDiff = 10000
 
     def setUp(self):
         self.portal = self.layer['portal']
@@ -29,7 +35,76 @@ class SearchTestCase(unittest.TestCase):
                 {
                     u'name': u'SearchableText',
                     u'prompt': u'SearchableText',
-                    u'value': u''
+                    u'value': u'',
+                },
+                {
+                    u'name': u'Title',
+                    u'prompt': u'Title',
+                    u'value': u'',
+                },
+                {
+                    u'name': u'Description',
+                    u'prompt': u'Description',
+                    u'value': u'',
+                },
+                {
+                    u'name': u'Subject',
+                    u'prompt': u'Subject',
+                    u'value': u'',
+                    u'options': [],
+                },
+                {
+                    u'name': u'portal_type',
+                    u'prompt': u'portal_type',
+                    u'value': u'',
+                    u'options': [],
+                },
+            ],
+        }})
+
+    def test_options(self):
+        portal = self.portal
+
+        setRoles(portal, TEST_USER_ID, ['Manager'])
+        login(portal, TEST_USER_NAME)
+        portal.invokeFactory('Document', 'testpage', title=u'Test Page')
+        workflowTool = getToolByName(portal, 'portal_workflow')
+        workflowTool.setDefaultChain("simple_publication_workflow")
+        workflowTool.doActionFor(portal.testpage, 'publish')
+
+        setRoles(portal, TEST_USER_ID, ['Member'])
+
+        f = search.JsonSearchPage(self.portal, self.request)
+        results = json.loads(f())
+        self.assertEqual(results,  {'collection': {
+            'version': '1.0',
+            'template': [
+                {
+                    u'name': u'SearchableText',
+                    u'prompt': u'SearchableText',
+                    u'value': u'',
+                },
+                {
+                    u'name': u'Title',
+                    u'prompt': u'Title',
+                    u'value': u'',
+                },
+                {
+                    u'name': u'Description',
+                    u'prompt': u'Description',
+                    u'value': u'',
+                },
+                {
+                    u'name': u'Subject',
+                    u'prompt': u'Subject',
+                    u'value': u'',
+                    u'options': [],
+                },
+                {
+                    u'name': u'portal_type',
+                    u'prompt': u'portal_type',
+                    u'value': u'',
+                    u'options': [{u'value': u'Document'}],
                 },
             ],
         }})
